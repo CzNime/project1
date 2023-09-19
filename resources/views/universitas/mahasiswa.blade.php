b9b1add2ac69cf21ebbc3ecbd5d2a1c3a9ab7f1d
Chandra Kusuma <intrusted01@gmail.com>
2023-09-20 07:52:43 +0800

    percobaan route

---
+++ resources/views/universitas/mahasiswa.blade.php
@@ -0,0 +1,7 @@
+<h2>Data mahasiswa</h2>
+
+<ul>
+    @foreach ($mahasiswa as $isi)
+        <li>{{ $isi }}</li>
+    @endforeach
+</ul>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return view('Hello world');
+});
+
+Route::get('/mahasiswa/{nama}/{jeniskelamin}', function ($nama, $jeniskelamin) {
+    return "Tampilkan data mahasiswa bernama $nama yang berjenis kelamin $jeniskelamin";
+});
+
+Route::get('/siswa', function(){
+    $array_nama = ["Milkyha", "Go yung joon", "Park bo young"];
+    return view('universitas.mahasiswa')->with('mahasiswa', $array_nama);
+});
+
+Route::get('/dosen/{namadosen}', function ($namadosen) {
+    return "$namadosen dosen galak, pelit nilai";
+});
